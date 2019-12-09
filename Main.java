7d45e1ab52f90316b3391ca23307f23624c1bd27
xivwong7110 <58575901+xivwong7110@users.noreply.github.com>
2019-12-10 07:37:59 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-
 package main;
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-      
             System.out.println("Hello World");
             Scanner obj = new Scanner (System.in);
             System.out.println("What is your name?:");
@@ -15,10 +10,8 @@ public class Main {
             String age = obj.nextLine();
             System.out.println("What's your address:");
             String add = obj.nextLine();
-            
             System.out.println("Hi! "+ name);
             System.out.println("You are "+ age);
             System.out.println("You live at "+ add);
-    }
-    
+    }   
 }
