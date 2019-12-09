376be6c4caf091eec8ee0b5d94ae1c9a21d1ab3b
xivwong7110 <58575901+xivwong7110@users.noreply.github.com>
2019-12-10 07:44:41 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+package main;
+import java.util.Scanner;
 public class Main {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+            Scanner obj = new Scanner (System.in);
+            System.out.println("What is your name?:");
+            String name = obj.nextLine();
+            System.out.println("How old are you?:");
+            String age = obj.nextLine();
+            System.out.println("What's your address:");
+            String add = obj.nextLine();
+            System.out.println("Hi! "+ name);
+            System.out.println("You are "+ age);
+            System.out.println("You live at "+ add);
+    }   
 }
